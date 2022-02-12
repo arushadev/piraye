9315502bfb19ef50cb4d11843128aaca8b82c169
Hamed <hamed.khaledi379@gmail.com>
2022-02-12 03:41:18 +0330

    add testing normalizer

---
+++ test/test_normalizer.py
@@ -0,0 +1,27 @@
+# testing Fibonacci number function
+# pylint: skip-file
+
+from normalizer import Normalizer
+
+
+def test_object():
+    norm = Normalizer(['alphabet_fa'])
+    assert norm is not None
+
+
+def test_not_changing_input():
+    text = "این یک متن فارسی ، English می‌باشد. این متن نباید تغییر کند.سال ۱۴۰۰ (1400)"
+    norm = Normalizer([])
+    assert text == norm.normalize(text)
+
+
+def test_number():
+    text = "1.2"
+    norm = Normalizer(['digit_fa'])
+    assert text != norm.normalize(text)
+
+
+def test_puncs():
+    text = "شروع ، پایان"
+    norm = Normalizer(['punc_en'])
+    assert text != norm.normalize(text)
