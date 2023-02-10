f440ef76ebe37d315a2af08da7a092dfe9b2766f
HosseiN Khademi Khaledi <hossein.khaledi@yahoo.com>
2023-02-10 05:25:37 +0330

    Add python 3.9 compatibility

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "Normalization",
     ],
     url="https://github.com/arushadev/piraye",
-    version="0.3.0",
+    version="0.3.1",
     package_dir={"piraye": "src"},
     packages=["piraye"],
     package_data={"piraye": ["data/*/*.json"]},
