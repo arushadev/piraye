5c352bfffd2643d88de033fb0aca03959cd92a41
Roozbahani <74303613+zroozbahani@users.noreply.github.com>
2023-11-25 16:48:04 +0330

    Update punctuation_restoration.py

--- piraye/tasks/punc/punctuation_restoration.py
+++ piraye/tasks/punc/punctuation_restoration.py
@@ -3,8 +3,12 @@ from abc import ABC
 from typing import List, Tuple
 
 from ...normalizer import Normalizer
-
-
+"""
+!pip install deepmultilingualpunctuation
+#deepmultilingualpunctuation-1.0.1-py3-none-any.whl (5.4 kB)
+!pip install transformers
+#transformers in /usr/local/lib/python3.10/dist-packages (4.35.2)
+"""
 class PunctuationRestoration(Normalizer, ABC):
     """
     Normalize text by restoring mission punctuation
@@ -29,3 +33,5 @@ class PunctuationRestoration(Normalizer, ABC):
         :return: A list of tuples containing the start index, end index, and punctuation token for each token span.
         """
         pass
+
+
